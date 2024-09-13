a5c3987976ea9f75c9129ce95b04587e42456499
ujjwal-git <26951554+ujjwal-git@users.noreply.github.com>
2024-09-13 12:46:02 +0530

    Django Models

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# class Model
+class Member(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dob = models.DateField(null=True)
+    phone = models.IntegerField(null=True)
+
+
