a335e1ef1cdeaae7167a3d41ad1f1195908de0fd
nimiyakujjwal <26951554+nimiyakujjwal@users.noreply.github.com>
2024-09-15 19:28:00 +0530

    Change DB to PostgreSQL

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pgsdwh',
+        'USER' : 'moon',
+        'PASSWORD' : '56789',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432'
     }
 }
 
